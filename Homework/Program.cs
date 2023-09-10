a4ec7835ae450e16c9fca7f84a27b9411a41cb9f
skari666 <144269531+skari666@users.noreply.github.com>
2023-09-10 20:34:43 +0500

    Добавьте файлы проекта.

---
+++ Homework/Program.cs
@@ -0,0 +1,15 @@
+﻿int newInt = 10;
+float newFloat = 8.4f;
+string newStr = "True";
+float bigInt = newInt * 3.5f;
+newFloat = newFloat - 1;
+string bigStr = newStr + "NoNoYesYesYes";
+Console.WriteLine("newInt = " + newInt);
+Console.WriteLine("bigInt = " + bigInt);
+Console.WriteLine("newStr = " + newStr);
+Console.WriteLine("bigStr = " + bigStr);
+Console.WriteLine("newFloat = " + newFloat);
+Console.WriteLine("bigStr = " + bigStr);
+Console.WriteLine("newInt / newFloat = " + newInt / newFloat);
+Console.WriteLine("bigInt / newFloat = " + bigInt / newFloat);
+Console.ReadKey();
